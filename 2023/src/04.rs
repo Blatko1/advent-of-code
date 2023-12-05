ff7a757fccf657ec0f4edc19d926ebf0d230dcbc
Blatko1 <kings.of.funny.gemplays@gmail.com>
2023-12-05 17:51:41 +0100

    solved day 5

--- 2023/src/04.rs
+++ 2023/src/04.rs
@@ -9,9 +9,16 @@ fn part1() {
     let mut sum = 0;
     for line in input.lines() {
         let content = line.split(':').last().unwrap().trim();
-        let split: Vec<_> = content.split('|').map(|nums| nums.trim()).collect();
-        let winning_numbers: Vec<u32> = split[0].split_whitespace().map(|num|num.parse().unwrap()).collect();
-        let mut available_numbers: Vec<u32> = split[1].split_whitespace().map(|num|num.parse().unwrap()).collect();
+        let split: Vec<_> =
+            content.split('|').map(|nums| nums.trim()).collect();
+        let winning_numbers: Vec<u32> = split[0]
+            .split_whitespace()
+            .map(|num| num.parse().unwrap())
+            .collect();
+        let mut available_numbers: Vec<u32> = split[1]
+            .split_whitespace()
+            .map(|num| num.parse().unwrap())
+            .collect();
         available_numbers.sort();
 
         let mut win_count = 0;
@@ -22,8 +29,8 @@ fn part1() {
         }
         match win_count.cmp(&1) {
             std::cmp::Ordering::Equal => sum += 1,
-            std::cmp::Ordering::Greater => sum += 2u32.pow(win_count-1),
-            _ => ()
+            std::cmp::Ordering::Greater => sum += 2u32.pow(win_count - 1),
+            _ => (),
         }
     }
     println!("sum: {}", sum);
@@ -35,9 +42,16 @@ fn part2() {
 
     for (card_i, line) in input.lines().enumerate() {
         let content = line.split(':').last().unwrap().trim();
-        let split: Vec<_> = content.split('|').map(|nums| nums.trim()).collect();
-        let winning_numbers: Vec<u32> = split[0].split_whitespace().map(|num|num.parse().unwrap()).collect();
-        let mut available_numbers: Vec<u32> = split[1].split_whitespace().map(|num|num.parse().unwrap()).collect();
+        let split: Vec<_> =
+            content.split('|').map(|nums| nums.trim()).collect();
+        let winning_numbers: Vec<u32> = split[0]
+            .split_whitespace()
+            .map(|num| num.parse().unwrap())
+            .collect();
+        let mut available_numbers: Vec<u32> = split[1]
+            .split_whitespace()
+            .map(|num| num.parse().unwrap())
+            .collect();
         available_numbers.sort();
 
         let mut win_count = 0;
@@ -46,10 +60,10 @@ fn part2() {
                 win_count += 1;
             }
         }
-        for i in (card_i+1)..=(card_i+win_count) {
+        for i in (card_i + 1)..=(card_i + win_count) {
             instance_state[i] += instance_state[card_i];
         }
     }
-    let sum: u32 =instance_state.iter().sum();
+    let sum: u32 = instance_state.iter().sum();
     println!("sum: {}", sum);
 }
---
+++ 2023/src/05.rs
@@ -0,0 +1,206 @@
+fn main() {
+    part1();
+    //part2();
+    part2_faster();
+}
+
+fn part1() {
+    let input = include_str!("../inputs/05.txt");
+
+    // Parse the input seeds from the 'input'
+    let input_seeds: Vec<u64> = input
+        .lines()
+        .next()
+        .unwrap()
+        .strip_prefix("seeds: ")
+        .unwrap()
+        .split_whitespace()
+        .map(|num| num.parse().unwrap())
+        .collect();
+
+    // Filter the input and get an array of map ranges (tuples)
+    let maps: Vec<Vec<(u64, u64, u64)>> = input
+        .split(':')
+        .skip(2)
+        .map(|map| {
+            let lines = map.lines().filter(|l| contains_digit(l));
+            lines
+                .map(|l| {
+                    let range: Vec<u64> = l
+                        .split_whitespace()
+                        .map(|num| num.parse().unwrap())
+                        .collect();
+                    (range[0], range[1], range[2])
+                })
+                .collect::<Vec<(u64, u64, u64)>>()
+        })
+        .collect();
+
+    let mut min = u64::MAX;
+    for seed in input_seeds {
+        let mut value = seed;
+
+        // Convert values incrementally following the map order
+        'maps: for map in maps.iter() {
+            // Check if the value is contained in map ranges
+            for range in map {
+                let dest_range_start = range.0;
+                let src_range_start = range.1;
+                let range_len = range.2;
+
+                // If the value is contained in a map range, convert it and continue to next map
+                if (src_range_start..(src_range_start + range_len))
+                    .contains(&value)
+                {
+                    value = dest_range_start + value - src_range_start;
+                    continue 'maps;
+                }
+            }
+        }
+        min = value.min(min);
+    }
+
+    println!("min location: {}", min);
+}
+
+/// This functions takes a very long time (2 - 3 min) to find the result. 
+fn part2() {
+    let input = include_str!("../inputs/05.txt");
+
+    // Parse the input seed ranges from the 'input'
+    let input_seed_ranges_str: &str = input
+        .lines()
+        .next()
+        .unwrap()
+        .strip_prefix("seeds: ")
+        .unwrap();
+
+    // Split the input seed range numbers into pairs and store as tuples
+    let mut seed_ranges: Vec<(u64, u64)> = Vec::new();
+    let ranges_split: Vec<u64> = input_seed_ranges_str
+        .split_whitespace()
+        .map(|num| num.parse().unwrap())
+        .collect();
+    for i in 0..(ranges_split.len() / 2) {
+        seed_ranges.push((ranges_split[i * 2], ranges_split[i * 2 + 1]))
+    }
+
+    // Filter the input and get an array of map ranges (tuples)
+    let maps: Vec<Vec<(u64, u64, u64)>> = input
+        .split(':')
+        .skip(2)
+        .map(|map| {
+            let lines = map.lines().filter(|l| contains_digit(l));
+            lines
+                .map(|l| {
+                    let range: Vec<u64> = l
+                        .split_whitespace()
+                        .map(|num| num.parse().unwrap())
+                        .collect();
+                    (range[0], range[1], range[2])
+                })
+                .collect::<Vec<(u64, u64, u64)>>()
+        })
+        .collect();
+
+    let mut min = u64::MAX;
+    for (seed_start, seed_range_len) in seed_ranges {
+        for seed in seed_start..(seed_start + seed_range_len) {
+            let mut value = seed;
+
+            // Convert values incrementally following the map order
+            for map in maps.iter() {
+                // Check if the value is contained in map ranges
+                for range in map {
+                    let dest_range_start = range.0;
+                    let src_range_start = range.1;
+                    let range_len = range.2;
+
+                    // If the value is contained in a map range, convert it and continue to next map
+                    if (src_range_start..(src_range_start + range_len))
+                        .contains(&value)
+                    {
+                        value = dest_range_start + value - src_range_start;
+                        break;
+                    }
+                }
+            }
+            min = value.min(min);
+        }
+    }
+
+    println!("min location: {}", min);
+}
+
+/// Much faster than the previous version (4 - 5 seconds)
+fn part2_faster() {
+    let input = include_str!("../inputs/05.txt");
+
+    // Parse the input seed ranges from the 'input'
+    let input_seed_ranges_str: &str = input
+        .lines()
+        .next()
+        .unwrap()
+        .strip_prefix("seeds: ")
+        .unwrap();
+
+    // Split the input seed range numbers into pairs and store as tuples
+    let mut seed_ranges: Vec<(u64, u64)> = Vec::new();
+    let ranges_split: Vec<u64> = input_seed_ranges_str
+        .split_whitespace()
+        .map(|num| num.parse().unwrap())
+        .collect();
+    for i in 0..(ranges_split.len() / 2) {
+        seed_ranges.push((ranges_split[i * 2], ranges_split[i * 2 + 1]))
+    }
+
+    // Filter the input and get an array of map ranges (tuples)
+    let maps: Vec<Vec<(u64, u64, u64)>> = input
+        .split(':')
+        .skip(2)
+        .map(|map| {
+            let lines = map.lines().filter(|l| contains_digit(l));
+            lines
+                .map(|l| {
+                    let range: Vec<u64> = l
+                        .split_whitespace()
+                        .map(|num| num.parse().unwrap())
+                        .collect();
+                    (range[0], range[1], range[2])
+                })
+                .collect::<Vec<(u64, u64, u64)>>()
+        })
+        .collect();
+
+    // Find the first existing minimum location
+    for location in 0..u64::MAX {
+        let mut value = location;
+        // Convert the location back into a seed
+        for map in maps.iter().rev() {
+            for range in map {
+                let dest_range_start = range.0;
+                let src_range_start = range.1;
+                let range_len = range.2;
+
+                // If the value is contained in a map range, convert it and continue to next map
+                if (dest_range_start..(dest_range_start + range_len))
+                    .contains(&value)
+                {
+                    value = src_range_start + value - dest_range_start;
+                    break;
+                }
+            }
+        }
+        if seed_ranges.iter().any(|&(seed_start, seed_range_len)| {
+            (seed_start..(seed_start + seed_range_len)).contains(&value)
+        }) {
+            println!("min location: {}", location);
+            return;
+        }
+    }
+    unreachable!()
+}
+
+fn contains_digit(l: &str) -> bool {
+    l.find(|c: char| c.is_ascii_digit()).is_some()
+}
